07ab11d0a67c6a7c04af54be547e6f00b761bde5
AKASH <akash.arthanur26@gmail.com>
2022-12-13 19:52:41 +0530

    all functionalities added

--- src/components/Home_page.jsx
+++ src/components/Home_page.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import Header from './header/header'
 import Cards from './posts/Cards'
 
 export default function Homepage() {
-
+    const [data, setdata] = useState([])
+    useEffect(() => {
+        axios.get("http://localhost:8000/instaclone/data").then((response) => { setdata(response.data.Posts) }).catch((error) => {
+            console.log(error)
+        })
+    }, [])
+    // console.log(data)
     return (
         <div>
             <Header />
-            <Cards />
+            {
+                data.map((val, i) => {
+                    return (
+                        <Cards name={val.name} location={val.location} likes={val.likes} description={val.description} date={val.date} PostImage={val.PostImage} key={i} />
+                    )
+                })
+            }
+
         </div>
     )
 }
--- src/components/createPost/NewPost.jsx
+++ src/components/createPost/NewPost.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Header from '../header/header'
 
 import "./NewPost.css"
 export default function NewPost() {
+    const history = useNavigate()
+    const [post, setpost] = useState({ PostImage: "", name: "", location: "", description: "" })
+    const [buffer, setbuffer] = useState(false)
+    const config = {
+        headers: {
+            "content-type": "multipart/form-data"
+        }
+    }
+    const handleClick = async (e) => {
+
+        e.preventDefault()
+        await axios.post("http://localhost:8000/instaclone/data", post, config).then((response) => {
+            console.log(response)
+        })
+        history("/instaclone")
+    }
     return (
         <>
             <Header />
@@ -10,16 +28,24 @@ export default function NewPost() {
                 <div id='file-wrap'>
                     <form method='POST'>
                         <div id='image_cont'>
-                            <input type="file" name="file" id="file" />
+                            <input type="file" name="PostImage" id="file" onChange={(e) => {
+                                setpost({ ...post, PostImage: e.target.files[0] })
+                            }} />
                         </div>
                         <div id='Auth_cont'>
-                            <input type="text" name="author" id="author" placeholder='Author' />
-                            <input type="text" name="Location" id="location" placeholder='Location' />
+                            <input type="text" name="name" id="author" placeholder='Author' onChange={(e) => {
+                                setpost({ ...post, name: e.target.value })
+                            }} />
+                            <input type="text" name="location" id="location" placeholder='Location' onChange={(e) => {
+                                setpost({ ...post, location: e.target.value })
+                            }} />
                         </div>
                         <div id='desc_cont'>
-                            <input type="text" name="description" id="description" placeholder='Description' />
+                            <input type="text" name="description" id="description" placeholder='Description' onChange={(e) => {
+                                setpost({ ...post, description: e.target.value })
+                            }} />
                         </div>
-                        <button id='post_butn' type='submit'>POST</button>
+                        <button id='post_butn' type='submit' onClick={handleClick}>POST</button>
                     </form>
                 </div>
             </div>
