e29fc4a1b3f7db262ab7b46cde53398ff7677db2
AKASH <akash.arthanur26@gmail.com>
2022-12-13 08:56:16 +0530

    Landing page ui created

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Homepage from "./components/Home_page";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/header";
 import NewPost from "./components/createPost/NewPost";
+import Landing_page from "./components/Landing page/landing_page";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route path="/" element={<Landing_page />} />
+          <Route path="/instaclone" element={<Homepage />} />
           <Route path="/add" element={<NewPost />} />
         </Routes>
       </Router>
